6e9b2ccdebe083518e9954c9e19b266bb7835dfc
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-03-20 16:53:33 -0300

    Update TI_DBConsole.sql

--- TI_DBConsole.sql
+++ TI_DBConsole.sql
@@ -1,11 +1,11 @@
-Select Initcap(Nvl(Gv$session.Username, '{Oracle}')), Gv$session.Sid, Gv$session.Serial#, Trim(To_Char(Trunc(Gv$session.Last_Call_Et / 60 / 60))) || ':' || 
-        Trim(To_Char(Trunc(Mod(Gv$session.Last_Call_Et, 3600) / 60), '00')) || ':' || 
-        Trim(To_Char(Mod(Mod(Gv$session.Last_Call_Et, 3600), 60), '00')) lastcall, Gv$instance.inst_id, Initcap(Gv$instance.instance_name), Decode(Gv$session.Blocking_Session, Null, Null, 'SID ' || Gv$session.Blocking_Session) , Initcap(Gv$session.Status) , Initcap(Gv$session.Osuser), Initcap(Gv$session.Machine), Initcap(Gv$session.Program), Initcap(Decode( 
-Dba_Scheduler_Running_Jobs.Job_Name, Null,  
-Decode(Dba_Jobs_Running.Job, Null, Gv$session.Module, 'Job ' || Dba_Jobs_Running.Job), 'Sch ' || dba_Scheduler_Running_Jobs.Job_Name)), Gv$session.Logon_time, Gv$session.Client_Info From Gv$session, Gv$sess_Io, Gv$process, Gv$instance, Dba_Jobs_Running, Dba_Scheduler_Running_Jobs                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          Where  Gv$session.Sid = Gv$sess_Io.Sid(+) 
-           And Gv$session.Inst_Id = Gv$sess_Io.Inst_Id(+) 
-           And Gv$session.Paddr = Gv$process.Addr(+) 
-           And Gv$session.Inst_Id = Gv$process.Inst_Id 
-           And Gv$session.Inst_Id = Gv$instance.Inst_Id 
-         And Gv$session.Sid = Dba_Jobs_Running.Sid(+) 
-         And Gv$session.Sid = Dba_Scheduler_Running_Jobs.Session_id(+) And Gv$session.Username Is Not Null And Gv$session.status = 'ACTIVE'  Order By Gv$session.sid
+SELECT INITCAP(NVL(GV$SESSION.USERNAME, '{Oracle}')), GV$SESSION.SID, GV$SESSION.SERIAL#, TRIM(TO_CHAR(TRUNC(GV$SESSION.LAST_CALL_ET / 60 / 60))) || ':' || 
+        TRIM(TO_CHAR(TRUNC(MOD(GV$SESSION.LAST_CALL_ET, 3600) / 60), '00')) || ':' || 
+        TRIM(TO_CHAR(MOD(MOD(GV$SESSION.LAST_CALL_ET, 3600), 60), '00')) LASTCALL, GV$INSTANCE.INST_ID, INITCAP(GV$INSTANCE.INSTANCE_NAME), DECODE(GV$SESSION.BLOCKING_SESSION, NULL, NULL, 'SID ' || GV$SESSION.BLOCKING_SESSION) , INITCAP(GV$SESSION.STATUS) , INITCAP(GV$SESSION.OSUSER), INITCAP(GV$SESSION.MACHINE), INITCAP(GV$SESSION.PROGRAM), INITCAP(DECODE( 
+DBA_SCHEDULER_RUNNING_JOBS.JOB_NAME, NULL,  
+DECODE(DBA_JOBS_RUNNING.JOB, NULL, GV$SESSION.MODULE, 'Job ' || DBA_JOBS_RUNNING.JOB), 'Sch ' || DBA_SCHEDULER_RUNNING_JOBS.JOB_NAME)), GV$SESSION.LOGON_TIME, GV$SESSION.CLIENT_INFO FROM GV$SESSION, GV$SESS_IO, GV$PROCESS, GV$INSTANCE, DBA_JOBS_RUNNING, DBA_SCHEDULER_RUNNING_JOBS                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          WHERE  GV$SESSION.SID = GV$SESS_IO.SID(+) 
+           AND GV$SESSION.INST_ID = GV$SESS_IO.INST_ID(+) 
+           AND GV$SESSION.PADDR = GV$PROCESS.ADDR(+) 
+           AND GV$SESSION.INST_ID = GV$PROCESS.INST_ID 
+           AND GV$SESSION.INST_ID = GV$INSTANCE.INST_ID 
+           AND GV$SESSION.SID = DBA_JOBS_RUNNING.SID(+) 
+           AND GV$SESSION.SID = DBA_SCHEDULER_RUNNING_JOBS.SESSION_ID(+) AND GV$SESSION.USERNAME IS NOT NULL AND GV$SESSION.STATUS = 'ACTIVE'  ORDER BY LASTCALL DESC
