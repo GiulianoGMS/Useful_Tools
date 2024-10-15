9f2641761f55284ad3606ae7d0349d0b14480334
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2024-10-15 13:55:57 -0300

    Update Cat Repetida API.sql

--- Cat Repetida API.sql
+++ Cat Repetida API.sql
@@ -5,17 +5,14 @@ SELECT DISTINCT 'CONSINCO_API' USU_INCLUSAO, F.SEQFAMILIA, SUBSTR( fNomeCompleto
   FROM MAP_FAMDIVCATEG Z INNER JOIN MAP_CATEGORIA C ON C.SEQCATEGORIA = Z.SEQCATEGORIA
                          INNER JOIN MAP_FAMILIA F ON F.SEQFAMILIA = Z.SEQFAMILIA
                          INNER JOIN (SELECT SEQFAMILIA, MAX(ZZ.DTABASEEXPORTACAO) DTAHORALTERACAO
-                         
-          FROM MAP_FAMDIVCATEG ZZ INNER JOIN MAP_CATEGORIA CC ON CC.SEQCATEGORIA = ZZ.SEQCATEGORIA
-              
-             WHERE STATUS = 'A'
-               AND CC.SEQCATEGORIA IN (20003,20004)
-               AND USUARIOALTERACAO = 'Consinco_API'
-               GROUP BY SEQFAMILIA) FF ON FF.SEQFAMILIA = F.SEQFAMILIA
+                                       FROM MAP_FAMDIVCATEG ZZ INNER JOIN MAP_CATEGORIA CC ON CC.SEQCATEGORIA = ZZ.SEQCATEGORIA
+                                      WHERE STATUS = 'A'
+                                        AND CC.SEQCATEGORIA IN (20003,20004)
+                                        AND USUARIOALTERACAO = 'Consinco_API'
+                                   GROUP BY SEQFAMILIA) FF ON FF.SEQFAMILIA = F.SEQFAMILIA
       
      WHERE STATUS = 'A'
        AND C.SEQCATEGORIA NOT IN (20003,20004)
-
 --=========================================================================================================================================--
 
        -- Old
