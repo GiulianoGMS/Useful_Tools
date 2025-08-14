25035ccee730ed4f8bd5f5cb3d6b245edec0ed0d
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2025-08-14 17:14:19 -0300

    Update Arred SI | CDs 504_505.sql

--- Arred SI | CDs 504_505.sql
+++ Arred SI | CDs 504_505.sql
@@ -1,15 +1,5 @@
 -- Altera a forma de arredondamento da SI quando a quantidade é menor nas empresas 504 e 505
--- Inicialmente, precisa alterar apenas na trigger abaixo
-
-tai_wm_mad_cargarecprod
-Linha 94
-         -- Giuliano 04/07/2025
-         -- CDs 504 e 505 arredondam qtd expedir cortada na x
-         CASE WHEN PEDEXP.NROEMPRESA IN (504,505) THEN ROUND(X.QTDEEXPEDIR)
-            ELSE X.QTDEEXPEDIR END,
-         ITEMEXP.TMPQTDEEXPEDIR)), 0) QUANTIDADE,
---
--- Talvez:
+-- Oficial Final:
 PKG_MLF_RECEBIMENTO.SP_CALCSELINVERSA
 Linha 14848
         (D.PESAVEL = 'N' or vtItem.NROEMPRESA IN (504,505)))
@@ -19,4 +9,12 @@ Linha 14188
         -- CDs 504/505 consideram qtd faltante no rateio como quantidade fechada e nao cracionada pela variavel vsPesavelSIN
         OR vtItem.NROEMPRESA IN (504,505))
 
---
+-- Talvez
+
+tai_wm_mad_cargarecprod
+Linha 94
+         -- Giuliano 04/07/2025
+         -- CDs 504 e 505 arredondam qtd expedir cortada na x
+         CASE WHEN PEDEXP.NROEMPRESA IN (504,505) THEN ROUND(X.QTDEEXPEDIR)
+            ELSE X.QTDEEXPEDIR END,
+         ITEMEXP.TMPQTDEEXPEDIR)), 0) QUANTIDADE,
