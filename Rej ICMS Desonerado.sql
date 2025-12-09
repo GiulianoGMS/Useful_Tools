85350859c36581027942ad48dbbe96c63a832f67
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2025-12-09 12:34:01 -0300

    Update Rej ICMS Desonerado.sql

--- Rej ICMS Desonerado.sql
+++ Rej ICMS Desonerado.sql
@@ -1,18 +1,12 @@
-Correção emissao com rejeicao ICMS DESONERADO
-Loja 39
- 
--- Descobre o SEQNF da nota pra buscar depois na tabela de item
- 
-SELECT DISTINCT X.SEQNF 
-  FROM MFL_DOCTOFISCAL
-WHERE X.NUMERODF = 32840 
-    AND X.NROEMPRESA = 39 
-    AND X.CODGERALOPER = 37
--- 381907274 < Codigo
--- Com esse codigo, faz o select na tabela de itens
- 
+-- Correção emissao com rejeicao ICMS DESONERADO
+-- Loja 39
+-- Descobre o SEQNF da nota pra buscar depois na tabela de item 
 -- Apagar os valores de INDMOTIVODESOICMS, VLRDESCICMS
  
-SELECT ROWID, VLRDESCICMS, INDMOTIVODESOICMS FROM MFL_DFITEM XI WHERE XI.SEQNF = 381907274;
-
--- Depois reenviar a nota
+SELECT ROWID, VLRDESCICMS, INDMOTIVODESOICMS
+  FROM MFL_DFITEM XI
+ WHERE XI.SEQNF = (SELECT DISTINCT X.SEQNF
+                     FROM MFL_DOCTOFISCAL X
+                    WHERE X.NUMERODF = 32964
+                          AND X.NROEMPRESA = 39
+                          AND X.CODGERALOPER = 37);
